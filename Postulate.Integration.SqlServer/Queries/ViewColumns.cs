4cb0ce94f034f57a39f637889ea38c89c136ad46
Adam O'Neil <adamosoftware@gmail.com>
2019-08-21 11:02:16 -0400

    Add project files.

---
+++ Postulate.Integration.SqlServer/Queries/ViewColumns.cs
@@ -0,0 +1,45 @@
+﻿using Postulate.Base;
+
+namespace SqlIntegration.Library.Queries
+{
+    public class ViewColumnsResult
+    {
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public int Size { get; set; }
+        public bool AllowNull { get; set; }
+
+        public string GetSyntax()
+        {
+            string sizeText = (Size == -1) ? "max" : string.Empty;
+            string typeName = (Size > 0) ? $"{DataType}({sizeText})" : DataType;
+            string nullable = (AllowNull) ? "NULL" : "NOT NULL";
+            return $"[{Name}] {typeName} {nullable}";
+        }
+    }
+
+    public class ViewColumns : Query<ViewColumnsResult>
+    {
+        public ViewColumns() : base(
+            @"SELECT 
+                [col].[name] AS [Name], 
+                TYPE_NAME([col].[system_type_id]) AS [DataType], 
+                CASE 
+                    WHEN TYPE_NAME([col].[system_type_id]) LIKE 'nvar%' THEN [col].[max_length]/2
+                    WHEN TYPE_NAME([col].[system_type_id]) LIKE 'var%' THEN [col].[max_length]
+                    ELSE 0
+                END AS [Size], 
+                [col].[is_nullable] AS [AllowNull]
+            FROM
+                [sys].[columns] [col]
+                INNER JOIN [sys].[views] [v] ON [col].[object_id]=[v].[object_id]
+            WHERE
+                SCHEMA_NAME([v].[schema_id])=@schemaName AND
+	            [v].[name]=@viewName")
+        {
+        }
+
+        public string SchemaName { get; set; }
+        public string ViewName { get; set; }
+    }
+}
---
+++ Postulate.Integration.SqlServer/Util.cs
@@ -0,0 +1,21 @@
+﻿using SqlIntegration.Library.Queries;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SqlIntegration.Library
+{
+    public static class Util
+    {
+        public static async Task<string> GetViewAsTableDefinitionAsync(
+            SqlConnection connection, 
+            string sourceSchema, string sourceView, string destSchema, string destTable)
+        {
+            var columns = await new ViewColumns() { SchemaName = sourceSchema, ViewName = sourceView }.ExecuteAsync(connection);
+
+            return $"CREATE TABLE [{destSchema}].[{destTable}] (\r\n\t" +
+                string.Join(",\r\n\t", columns.Select(col => col.GetSyntax())) +
+                "\r\n)";
+        }
+    }
+}
