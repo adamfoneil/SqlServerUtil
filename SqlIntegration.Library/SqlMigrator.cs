e58bd6a3b86933d7cc0060548c6e0f382ce2eb2b
Adam O'Neil <adamosoftware@gmail.com>
2019-12-21 11:06:30 -0500

    progress on migrator

--- SqlIntegration.Library/SqlMigrator.cs
+++ SqlIntegration.Library/SqlMigrator.cs
@@ -4,7 +4,9 @@ using SqlIntegration.Library.Extensions;
 using SqlIntegration.Library.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SqlIntegration.Library
@@ -15,7 +17,10 @@ namespace SqlIntegration.Library
         private const string JobTable = "Job";
         private const string KeyMapTable = "KeyMap";
 
-        public SqlMigrator(int jobId) { JobId = jobId; }
+        public SqlMigrator(int jobId) 
+        { 
+            JobId = jobId; 
+        }
 
         public int JobId { get; }
 
@@ -64,11 +69,12 @@ namespace SqlIntegration.Library
                 await cn.ExecuteAsync($@"CREATE TABLE [{Schema}].[{tableName}] (
                     [JobId] int NOT NULL,
                     [Timestamp] datetime NOT NULL DEFAULT (getutcdate()),
+                    [Schema] nvarchar(100) NOT NULL,
                     [TableName] nvarchar(100) NOT NULL,
                     [SourceId] {info.SqlTypeName} NOT NULL,
                     [NewId] {info.SqlTypeName} NOT NULL,
-                    CONSTRAINT [PK_KeyMap_{info.MapTableSuffix}] PRIMARY KEY ([TableName], [SourceId]),
-                    CONSTRAINT [U_KeyMap_{info.MapTableSuffix}_NewId] UNIQUE ([TableName], [NewId]),
+                    CONSTRAINT [PK_KeyMap_{info.MapTableSuffix}] PRIMARY KEY ([Schema], [TableName], [SourceId]),
+                    CONSTRAINT [U_KeyMap_{info.MapTableSuffix}_NewId] UNIQUE ([Schema], [TableName], [NewId]),
                     CONSTRAINT [FK_KeyMap_{info.MapTableSuffix}_Job] FOREIGN KEY ([JobId]) REFERENCES [{Schema}].[{JobTable}] ([Id])
                 )");
             }
@@ -81,19 +87,125 @@ namespace SqlIntegration.Library
 
         private static string GetTableName(out KeyMapTableInfo info)
         {
-            info = SupportedIdentityTypes[typeof(TIdentity)];
-            return $"{KeyMapTable}_{info.MapTableSuffix}";
+            try
+            {
+                info = SupportedIdentityTypes[typeof(TIdentity)];
+                return $"{KeyMapTable}_{info.MapTableSuffix}";
+            }
+            catch (Exception exc)
+            {
+                string supportedTypes = string.Join(", ", SupportedIdentityTypes.Keys.Select(k => k.Name));
+                throw new Exception($"Error getting KeyMap table name: {exc.Message}. Make sure you're using a supported type: {supportedTypes}");
+            }
         }
 
-        public async Task CopySelfAsync(
-            SqlConnection connection, string schema, string tableName, string criteria = null, object parameters = null, 
-            Dictionary<string, string> setForeignKeys = null,
+        public async Task CopyRowsAsync(
+            SqlConnection connection, 
+            string schema, string tableName, DataTable dataTable, string identityColumn, 
+            Dictionary<string, string> mapForeignKeys = null,
             Dictionary<string, object> setColumns = null)
+        {
+            var mappingCmd = await SqlServerCmd.FromTableSchemaAsync(connection, Schema, GetTableName());
+            mappingCmd["JobId"] = JobId;
+            mappingCmd["Schema"] = schema;
+            mappingCmd["TableName"] = tableName;
+
+            var cmd = await SqlServerCmd.FromTableSchemaAsync(connection, schema, tableName);
+            
+            await ValidateForeignKeyMappingAsync(connection, mapForeignKeys);
+            ValidateSetColumns(setColumns, cmd);            
+            EnsureNoColumnNameOverlap(mapForeignKeys, setColumns);
+
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                TIdentity sourceId = dataRow.Field<TIdentity>(identityColumn);
+                
+                // has this row already been mapped?
+                if (await RowIsMappedAsync(connection, schema, tableName, sourceId)) continue;
+                
+                cmd.BindDataRow(dataRow);
+                if (mapForeignKeys != null) MapForeignKeys(dataRow, mapForeignKeys);
+                if (setColumns != null) SetColumns(dataRow, setColumns);
+
+                try
+                {
+                    // copy the source row to the destination connection
+                    TIdentity newId = await cmd.InsertAsync<TIdentity>(connection);
+
+                    try
+                    {
+                        mappingCmd["SourceId"] = sourceId;
+                        mappingCmd["NewId"] = newId;
+                        await mappingCmd.InsertAsync<TIdentity>(connection);
+                    }
+                    catch
+                    {
+                        // if there's a mapping problem, we need to quit right away and diagnose
+                        throw;
+                    }
+                }
+                catch (Exception exc)
+                {
+                    // insert errors should be logged with the job
+                    throw;
+                }
+            }
+        }
+
+        private void EnsureNoColumnNameOverlap(Dictionary<string, string> mapForeignKeys, Dictionary<string, object> setColumns)
+        {
+            if (mapForeignKeys == null) return;
+            if (setColumns == null) return;
+
+            var overlap = from fk in mapForeignKeys.Keys
+                          join col in setColumns.Keys on fk.ToLower() equals col.ToLower()
+                          select fk;
+
+            if (overlap.Any())
+            {
+                string keyList = string.Join(", ", overlap);
+                throw new Exception($"Can't have overlapping foreign keys and columns: {keyList}");
+            }
+        }
+
+        private void ValidateSetColumns(Dictionary<string, object> setColumns, SqlServerCmd cmd)
         {
             throw new NotImplementedException();
         }
 
-        public async Task CopyToAsync(
+        private async Task ValidateForeignKeyMappingAsync(SqlConnection connection, Dictionary<string, string> mapForeignKeys)
+        {
+            if (mapForeignKeys == null) return;
+
+            var validSources = await connection.QueryAsync<string>(
+                $@"SELECT [Schema] + '.' + [TableName] AS [Table]
+                FROM {GetTableName()}
+                GROUP BY [Schema], [TableName]");
+
+            var invalid = mapForeignKeys.Keys.Except(validSources);
+            if (invalid.Any())
+            {
+                string invalidKeys = string.Join(", ", invalid);
+                throw new Exception($"There are one or more unrecognized foreign key mappings: {invalidKeys}");
+            }
+        }
+
+        private void SetColumns(DataRow dataRow, Dictionary<string, object> setColumns)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MapForeignKeys(DataRow dataRow, Dictionary<string, string> mapForeignKeys)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task<bool> RowIsMappedAsync(SqlConnection connection, string schema, string tableName, TIdentity idValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task CopyAcrossAsync(
             SqlConnection fromConnection, SqlConnection toConnection, string schema, string tableName, string criteria = null, object parameters = null,
             Dictionary<string, string> setForeignKeys = null,
             Dictionary<string, object> setColumns = null)
