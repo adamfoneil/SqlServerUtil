301a9b4ecd23d8e2703dd1fd29ab47db550d48fc
Adam ONeil <adamosoftware@gmail.com>
2020-02-07 14:13:39 -0500

    started on CacheManager

---
+++ SqlIntegration.Library/CacheManager.cs
@@ -0,0 +1,55 @@
+﻿using Dapper;
+using SqlIntegration.Library.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SqlIntegration.Library
+{
+    /// <summary>
+    /// query helper that caches expensive query results in a temp table,
+    /// automatically refreshing the underlying data when it becomes invalid
+    /// </summary>
+    public abstract class CacheManager<T> where T : ICacheRow
+    {
+        /// <summary>
+        /// how do we get the latest data for a list of Ts
+        /// </summary>
+        protected abstract Task<IEnumerable<T>> GetUpdatesAsync(SqlConnection connection, IEnumerable<T> currentRows);
+
+        protected abstract string BaseQuery { get; }
+
+        protected abstract TimeSpan RecalcAfter { get; }
+
+        public async Task<IEnumerable<T>> QueryAsync(SqlConnection connection, string criteria, object parameters)
+        {
+            var results = new List<T>();
+            var queryResults = await connection.QueryAsync<T>(BaseQuery + " WHERE " + criteria, parameters);
+
+            results.AddRange(queryResults.Where(row => !row.IsInvalid));
+
+            var invalidRows = queryResults.Where(row => IsInvalid(row));
+            if (invalidRows.Any())
+            {
+                var updates = await GetUpdatesAsync(connection, invalidRows);
+            }
+            
+            return results;            
+        }
+
+        private bool IsInvalid(ICacheRow row)
+        {
+            if (row.IsInvalid)
+            {
+                if (row.LastRefreshed.HasValue && RecalcAfter > TimeSpan.Zero)
+                {
+                    return DateTime.UtcNow.Subtract(row.LastRefreshed.Value) > RecalcAfter;
+                }                    
+            }
+
+            return false;
+        }
+    }
+}
---
+++ SqlIntegration.Library/Interfaces/ICacheRow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SqlIntegration.Library.Interfaces
+{
+    /// <summary>
+    /// describes a row that can be marked invalid
+    /// </summary>
+    public interface ICacheRow
+    {
+        /// <summary>
+        /// if true, data is considered out of date and needs refreshing
+        /// </summary>
+        bool IsInvalid { get; }
+
+        /// <summary>
+        /// when was this row last updated? UTC time
+        /// </summary>
+        DateTime? LastRefreshed { get; }
+    }
+}
